0682c896c99954b0bff70568c921b29166a706ab
Ricardo Lopez <rikyfocil@gmail.com>
2017-02-23 22:42:28 -0600

    commit temporal. Ignorar gracias

---
+++ app/Http/Controllers/ClientesController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Cliente;
+use App\Estado;
+
+class ClientesController extends Controller
+{
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('clientes.create', ['cliente' => null, 'estados' => Estado::all()]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $cliente = Cliente::where('id', $id)->firstOrFail();
+        return view('clientes.create', ['cliente' => $cliente, 'direccion' => $cliente->direccion(), 'estados' => Estado::all()]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $cliente = Cliente::where('id', $id)->firstOrFail();
+        $cliente->visible = 0;
+        $cliente->save();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/login', function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::resource('clientes', 'ClientesController');
